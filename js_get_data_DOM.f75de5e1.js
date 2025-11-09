461b51003b888edfc9bcfb5a03c611c5687c2282
Andrey <inokatre08@gmail.com>
2025-11-09 19:17:38 +0200

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,17 @@
+'use strict';
+const elements = [
+    ...document.querySelectorAll('.population')
+].map((a)=>{
+    const num = Number(a.textContent.split(',').join(''));
+    return !isNaN(num) ? num : 0;
+}); // eslint-disable-line
+const total = elements.reduce((a, b)=>a + b);
+const avrg = Math.round(total / elements.length);
+const finishTotal = total.toLocaleString('en-US');
+const finishAvrg = avrg.toLocaleString('en-US');
+const totalHtml = document.querySelector('.total-population');
+const avrgHtml = document.querySelector('.average-population');
+totalHtml.textContent = finishTotal;
+avrgHtml.textContent = finishAvrg;
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
