e6b161230149b418e1c4834c525fdc38cd10ce8f
Andrey <inokatre08@gmail.com>
2025-11-09 18:51:23 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const elements = [...document.querySelectorAll('.population')].map((a) =>
+  Number(a.textContent.split(',').join('')),
+); // eslint-disable-line
+
+const total = elements.reduce((a, b) => a + b);
+const avrg = Math.round(total / elements.length);
+const finishTotal = total.toLocaleString('en-US');
+const finishAvrg = avrg.toLocaleString('en-US');
+const totalHtml = document.querySelector('.total-population');
+const avrgHtml = document.querySelector('.average-population');
+
+totalHtml.replaceWith(finishTotal);
+avrgHtml.replaceWith(finishAvrg);
